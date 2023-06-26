96d0bc912f6c3ee282a6760c4bc22855936f7db2
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-06-26 22:43:04 +0200

    Use a sophisticated cli parser (#5)

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,34 +1,18 @@
-import io.ktor.client.call.body
-import io.ktor.client.plugins.resources.get
 import io.ktor.resources.Resource
-import io.ktor.utils.io.core.use
-import kotlinx.coroutines.runBlocking
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 fun main(arguments: Array<String>) {
-    val firstArgument = arguments.firstOrNull() ?: throw IllegalArgumentException("The first argument must be the key of the Jira issue")
+    val parser = ArgParser("TicketChimp")
+    val setup by parser.option(ArgType.Boolean, shortName = "s", description = "Starts the setup", fullName = "setup").default(true)
+    parser.parse(arguments)
 
-    val setupManager = PosixSetupManager()
-    if (firstArgument.startsWith("-")) {
-        setupManager.launchSetup()
-    }
-    require(setupManager.configExists) {
-        "Config file does not exist"
-    }
-    val config = setupManager.readConfig()
-    setupManager.close()
-
-    val httpClient = createKtorHttpClient(
-        tokenKey = config.tokenKey ?: throw IllegalArgumentException("No token key provided"),
-        host = "host",
-    )
-
-    httpClient.use {
-        runBlocking {
-            val issue: Issue = it.get(Ticket(firstArgument)).body()
-            println("key = ${issue.key}")
-        }
+    if (setup) {
+        val setupManager = PosixSetupManager()
+        setupManager.startSetup()
     }
 }
 
