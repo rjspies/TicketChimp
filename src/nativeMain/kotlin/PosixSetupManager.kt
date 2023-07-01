4ea5bc6db22eff7bfa281adcb978a57b8cddcf47
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-07-01 22:19:10 +0200

    Created capability to store the config in ticketchimp.json (#6)

--- src/nativeMain/kotlin/PosixSetupManager.kt
+++ src/nativeMain/kotlin/PosixSetupManager.kt
@@ -1,29 +1,66 @@
+import kotlinx.cinterop.toKString
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import platform.posix.getenv
 
 class PosixSetupManager {
-    private fun askAuth(): AuthType {
+    private fun askAuth(): AuthType? {
         println("Use basic or bearer auth? (basic/bearer):")
         return when (readlnOrNull()) {
             "basic" -> AuthType.Basic
             "bearer" -> AuthType.Bearer
-            else -> AuthType.Undefined
+            else -> null
         }
     }
 
+    private fun askHost(): String? {
+        println("Jira host? (e. g. \"company.atlassian.net\"):")
+        return readlnOrNull()
+    }
+
+    private fun askUsername(): String? {
+        println("Username?:")
+        return readlnOrNull()
+    }
+
+    private fun askTokenKey(basicAuth: Boolean): String? {
+        println("Token key stored in your environment variables? (not the actual token value):")
+        if (basicAuth) println("Basic auth also uses the token instead of a password.")
+        return readlnOrNull()
+    }
+
     fun startSetup() {
+        val host = askHost()
         val authType = askAuth()
-        println(authType)
+        val basicAuth = authType == AuthType.Basic
+        val username = if (basicAuth) askUsername() else null
+        val tokenKey = askTokenKey(basicAuth)
+        val config = Config(
+            host = host,
+            authType = authType,
+            username = username,
+            tokenKey = tokenKey,
+        )
+        println("${getenv("HOME")?.toKString()}/.config/ticketchimp.json")
+        FileSystem.SYSTEM.write("${getenv("HOME")?.toKString()}/.config/ticketchimp.json".toPath()) {
+            val json = Json { prettyPrint = true }
+            writeUtf8(json.encodeToString(config))
+        }
     }
 }
 
 enum class AuthType {
     Basic,
     Bearer,
-    Undefined,
 }
 
 @Serializable
 data class Config(
-    val bearerAuth: Boolean,
+    val host: String?,
+    val authType: AuthType?,
+    val username: String?,
     val tokenKey: String?,
 )
